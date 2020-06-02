55610472c5ab599ea8db82b51b674f01d55a7fc6
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-06-02 15:08:01 +0200

    improved ctors

--- CryptoAnalysis-Android/src/main/java/de/fraunhofer/iem/crypto/CogniCryptAndroidAnalysis.java
+++ CryptoAnalysis-Android/src/main/java/de/fraunhofer/iem/crypto/CogniCryptAndroidAnalysis.java
@@ -1,23 +1,31 @@
 package de.fraunhofer.iem.crypto;
 
+import java.io.File;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import com.google.common.collect.Lists;
+import com.google.inject.internal.util.$Nullable;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import boomerang.callgraph.BoomerangICFG;
 import boomerang.callgraph.ObservableICFG;
 import boomerang.callgraph.ObservableStaticICFG;
 import boomerang.preanalysis.BoomerangPretransformer;
-import com.google.common.collect.Lists;
 import crypto.analysis.CrySLResultsReporter;
 import crypto.analysis.CryptoScanner;
 import crypto.analysis.errors.AbstractError;
 import crypto.cryslhandler.CrySLModelReader;
-import crypto.cryslhandler.CryslReaderUtils;
 import crypto.exceptions.CryptoAnalysisException;
 import crypto.reporting.CollectErrorListener;
+import crypto.reporting.CommandLineReporter;
 import crypto.rules.CrySLRule;
 import crypto.rules.CrySLRuleReader;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
@@ -28,21 +36,13 @@ import soot.jimple.infoflow.android.SetupApplication;
 import soot.jimple.infoflow.android.config.SootConfigForAndroid;
 import soot.options.Options;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
-import crypto.reporting.CommandLineReporter;
-
 public class CogniCryptAndroidAnalysis {
 
 	private static final Logger logger = LoggerFactory.getLogger(CogniCryptAndroidAnalysis.class);
 	private File apkFile;
 	private File platformsDirectory;
 	private File rulesLocation;
+	private File outputDir;
 	private final Collection<String> applicationClassFilter;
 
 	/**
@@ -91,23 +91,41 @@ public class CogniCryptAndroidAnalysis {
 		return applicationClassFilter;
 	}
 
-	/**
-	 * @param apkFile The Android platforms directory.
-	 * @param platformsDirectory The Android platform which the .apk targets.
-	 * @param rulesLocation The CrySL rules directory.
-	 */
-	public CogniCryptAndroidAnalysis(File apkFile, File platformsDirectory, File rulesLocation) {
-		this(apkFile, platformsDirectory, rulesLocation, Collections.EMPTY_LIST);
+	public Optional<File> getOutputDir() {
+		return Optional.ofNullable(outputDir);
+	}
+	private void setOutputDir(File file) {
+		outputDir = file;
 	}
 
 	/**
 	 * @param apkFile The Android platforms directory.
 	 * @param platformsDirectory The Android platform which the .apk targets.
 	 * @param rulesLocation The CrySL rules directory.
+	 */
+	public CogniCryptAndroidAnalysis(File apkFile, File platformsDirectory, File rulesLocation) {
+		this(apkFile, platformsDirectory, rulesLocation, null, Collections.EMPTY_LIST);
+	}
+
+	/**
+	 * @param apkFile The Android platforms directory.
+	 * @param platformsDirectory The Android platform which the .apk targets.
+	 * @param outputDir The path where output files get saved.
+	 * @param rulesLocation The CrySL rules directory.
+	 */
+	public CogniCryptAndroidAnalysis(File apkFile, File platformsDirectory, File rulesLocation, File outputDir) {
+		this(apkFile, platformsDirectory, rulesLocation, outputDir, Collections.EMPTY_LIST);
+	}
+
+	/**
+	 * @param apkFile The Android platforms directory.
+	 * @param platformsDirectory The Android platform which the .apk targets.
+	 * @param rulesLocation The CrySL rules directory.
+	 * @param outputDir The path where output files get saved.
 	 * @param applicationClassFilter Collection of full qualified class names the analysis shall analyze explicitly.
 	 *                               Settings this can increase analysis runtime. If null or empty this parameter gets ignored.
 	 */
-	public CogniCryptAndroidAnalysis(File apkFile, File platformsDirectory, File rulesLocation,
+	public CogniCryptAndroidAnalysis(File apkFile, File platformsDirectory, File rulesLocation, File outputDir,
 									 Collection<String> applicationClassFilter) {
 		if (apkFile == null)
 			throw new IllegalArgumentException("Argument 'apkFile' must not be null.");
@@ -119,6 +137,7 @@ public class CogniCryptAndroidAnalysis {
 		setApkFile(apkFile);
 		setPlatformsDirectory(platformsDirectory);
 		setRulesLocation(rulesLocation);
+		setOutputDir(outputDir);
 
 		if (applicationClassFilter == null)
 			this.applicationClassFilter = Collections.EMPTY_LIST;
@@ -132,18 +151,19 @@ public class CogniCryptAndroidAnalysis {
 	 * @param rulesLocation Absolute path of the CrySL rules directory.
 	 */
 	public CogniCryptAndroidAnalysis(String apkFile, String platformsDirectory, String rulesLocation) {
-		this(apkFile, platformsDirectory, rulesLocation, Collections.EMPTY_LIST);
+		this(apkFile, platformsDirectory, rulesLocation, null, Collections.EMPTY_LIST);
 	}
 
 	/**
 	 * @param apkFile Absolute path of the Android platform which the .apk targets.
 	 * @param platformsDirectory Absolute path of the Android platform which the .apk targets.
 	 * @param rulesLocation Absolute path of the CrySL rules directory.
+	 * @param outputDir The path where output files get saved.
 	 * @param applicationClassFilter Collection of full qualified class names the analysis shall analyze explicitly.
 	 *                               Settings this can increase analysis runtime. If null or empty this parameter gets ignored.
 	 */
 	public CogniCryptAndroidAnalysis(String apkFile, String platformsDirectory, String rulesLocation,
-									 Collection<String> applicationClassFilter) {
+									 @$Nullable String outputDir, Collection<String> applicationClassFilter) {
 		if (StringUtils.isBlank(apkFile))
 			throw new IllegalArgumentException("Argument 'apkFile' must not be null or empty.");
 		if (StringUtils.isBlank(platformsDirectory))
@@ -164,14 +184,20 @@ public class CogniCryptAndroidAnalysis {
 	public static void main(String... args) {
 		CogniCryptAndroidAnalysis analysis;
 		if (args[3] != null) {
-			analysis = new CogniCryptAndroidAnalysis(args[0], args[1], args[2], args[3], Lists.<String>newArrayList());
+			analysis = new CogniCryptAndroidAnalysis(args[0], args[1], args[2], args[3], Lists.newArrayList());
 		} else {
-			analysis = new CogniCryptAndroidAnalysis(args[0], args[1], args[2], Lists.<String>newArrayList());
+			analysis = new CogniCryptAndroidAnalysis(args[0], args[1], args[2], null, Lists.newArrayList());
 		}
-		analysis.run();
+		try {
+			analysis.run();
+		} catch (CryptoAnalysisException e) {
+			String message = "Analysis failed with error: " + e.getMessage();
+			logger.error(message, e);
+			System.out.println(message);
+		}
 	}
 
-	public Collection<AbstractError> run() {
+	public Collection<AbstractError> run() throws CryptoAnalysisException {
 		logger.info("Running static analysis on APK file " + apkFile);
 		logger.info("with Android Platforms dir " + platformsDirectory);
 		constructCallGraph();
@@ -199,7 +225,7 @@ public class CogniCryptAndroidAnalysis {
 		logger.info("Done constructing call graph");
 	}
 
-	private Collection<AbstractError> runCryptoAnalysis() {
+	private Collection<AbstractError> runCryptoAnalysis() throws CryptoAnalysisException {
 		prepareAnalysis();
 
 		final ObservableStaticICFG icfg = new ObservableStaticICFG(new BoomerangICFG(false));
@@ -208,7 +234,10 @@ public class CogniCryptAndroidAnalysis {
 		final CrySLResultsReporter reporter = new CrySLResultsReporter();
 		CollectErrorListener errorListener = new CollectErrorListener();
 		reporter.addReportListener(errorListener);
-		reporter.addReportListener(new CommandLineReporter(outputDir, rules));
+		if (outputDir != null) {
+			reporter.addReportListener(new CommandLineReporter(outputDir.getAbsolutePath(), rules));
+		}
+
 		CryptoScanner scanner = new CryptoScanner() {
 
 			@Override
@@ -251,7 +280,7 @@ public class CogniCryptAndroidAnalysis {
 		logger.info("Library classes: "+ Scene.v().getLibraryClasses().size());
 	}
 
-	protected List<CrySLRule> getRules() {
+	protected List<CrySLRule> getRules() throws CryptoAnalysisException {
 
 		if (rulesLocation == null) {
 			// TODO: Create a custom CryptoAnalysisException (or CogniCryptException)
@@ -261,19 +290,16 @@ public class CogniCryptAndroidAnalysis {
 		}
 
 		List<CrySLRule> rules;
-		if (rulesLocation.isDirectory()){
-			// TODO: Add option for recursive check
-			rules =  CrySLRuleReader.readFromDirectory(rulesLocation);
-		}
-		else if (rulesLocation.getName().endsWith(".zip"))
-		{
-			rules = CrySLRuleReader.readFromZipFile(rulesLocation);
-		}
+		// TODO: Add option for recursive check
 		// TODO: Support explicit .crySL file as input
-		else
+		if (rulesLocation.isDirectory()) {
+			rules =  CrySLRuleReader.readFromDirectory(rulesLocation);
+		} else if (rulesLocation.getName().endsWith(".zip")) {
+			rules = CrySLRuleReader.readFromZipFile(rulesLocation);
+		} else {
 			throw new RuntimeException(
 					"Please specify a directory the CrySL rules (" + CrySLModelReader.cryslFileEnding + " Files) are located in.");
-
+		}
 
 		if (rules.isEmpty())
 			System.out.println("CogniCrypt did not find any rules to start the analysis for. \n "
